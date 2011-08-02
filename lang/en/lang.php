71ee1e93dce73cd39f3931fff63775d52f1998a3
Hendry Badao <hendrybadao@gmail.com>
2011-08-02 21:02:07 +0300

    Initial commit -- Export from Fossil SCM (2011-Aug-2)

---
+++ lang/en/lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Symon Bent <symonbent@gmail.com>
+ */
+
+// custom language strings for the plugin
+$lang['subjectindex_prefix'] = '(Index Entry)  ';
+$lang['subjectindex_tag'] = '(Tag)  ';
+$lang['link_to_top']  = 'Top ↑';
+$lang['empty_index'] = 'The Subject Index is empty!';
+?>
