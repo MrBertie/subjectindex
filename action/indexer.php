0476f935c73c0331b757c54e791a68bc64909ff4
Hendry Badao <hendrybadao@gmail.com>
2012-01-18 21:20:18 +0300

    Removed unnecessary debugging, logging code

--- action/indexer.php
+++ action/indexer.php
@@ -8,7 +8,6 @@
 if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
-require_once(DOKU_INC.'inc/logger.php');
 require_once(DOKU_PLUGIN . 'subjectindex/inc/matcher.php');
 
 class action_plugin_subjectindex_indexer extends DokuWiki_Action_Plugin {
